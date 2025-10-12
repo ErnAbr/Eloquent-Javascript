76e71c6f969b1efd47d302d650a4ddc1fb78cd9c
ErnAbr <ern_abr@yahoo.com>
2025-10-12 09:57:02 +0300

    #2 Quoting Style

--- lessons/09-RegularExpresions.js
+++ lessons/09-RegularExpresions.js
@@ -10,16 +10,27 @@
 // #1 Regexp Golf
 
 const first = /ca[rt]/;
-console.log(first.test("my car"));
+// console.log(first.test("my car"));
 const second = /pr?op/;
-console.log(second.test("lets prop"));
+// console.log(second.test("lets prop"));
 const third = /fer[a-z]/;
-console.log(third.test("brand new ferrary"));
+// console.log(third.test("brand new ferrary"));
 const fourth = /[a-z]ious/;
-console.log(fourth.test("ruinous"));
+// console.log(fourth.test("ruinous"));
 const fifth = /\s[.,:;]/;
-console.log(fifth.test("bad punctuation"));
+// console.log(fifth.test("bad punctuation"));
 const sixth = /.{6}/;
-console.log(sixth.test("Siebentausenddreihundertzweiundzwanzig"));
+// console.log(sixth.test("Siebentausenddreihundertzweiundzwanzig"));
 const seventh = /\b[^eE\s]+\b/g;
-console.log(seventh.test("wobbling nest"));
+// console.log(seventh.test("wobbling nest"));
+
+// #2 Quoting Style
+
+const regex2 = /'([^']*)'/g;
+const regex3 = /\b[a-z]'[a-z]|'([^']*)'\b/i;
+const text = "we ain't going 'nowhere'";
+
+console.log(text.replace(regex3, "$1", "$2"));
+
+const result = text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2');
+console.log(result);
